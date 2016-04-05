90b0520d2627b9d95e59a08bd7926c9a12a92415
Tony Won <tonywon89@gmail.com>
2016-04-05 13:52:46 -0700

    Implement session initialize

--- lib/session.rb
+++ lib/session.rb
@@ -1,12 +1,18 @@
 require 'json'
 
 class Session
+
+  attr_reader :session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    json_cookies = req.cookies['_rails_lite_app']
+    parsed_cookies = JSON.parse(json_cookies)
+    @session =  parsed_cookies
   end
 
   def [](key)
+    session[key]
   end
 
   def []=(key, val)
