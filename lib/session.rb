bd4d22247a8f426bf0f8122322543f78f39e28f7
Tony Won <tonywon89@gmail.com>
2016-04-05 14:02:14 -0700

    Implement store_session

--- lib/session.rb
+++ lib/session.rb
@@ -6,9 +6,9 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
-    json_cookies = req.cookies['_rails_lite_app']
+    json_cookies = req.cookies['_rails_lite_app'] || {}.to_json
     parsed_cookies = JSON.parse(json_cookies)
-    @session =  parsed_cookies
+    @session = parsed_cookies
   end
 
   def [](key)
@@ -16,10 +16,18 @@ class Session
   end
 
   def []=(key, val)
+    session[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie(
+      '_rails_lite_app',
+      {
+        path: '/',
+        value: session.to_json
+      }
+    )
   end
 end
