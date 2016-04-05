f6349d78eb3472d8fcd1cb7558636777e0fbde4a
Tony Won <tonywon89@gmail.com>
2016-04-05 11:38:13 -0700

    Implement render_content and already_built_response

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,10 +8,13 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    !@already_built_response.nil?
   end
 
   # Set the response status code and header
@@ -22,6 +25,11 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise if already_built_response?
+    res['Content-Type'] = content_type
+    res.write(content)
+    @already_built_response = res
+
   end
 
   # use ERB and binding to evaluate templates
@@ -37,4 +45,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
